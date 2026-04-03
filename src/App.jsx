14433b7758cf214bc76f1d5418c78b4420c7996b
mike7418 <parksmike@dwx.com>
2026-04-03 10:24:24 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import Nav from './components/Nav'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+          <Nav />
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import LibraryLogo from './assets/images/Library.svg'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  
+    <App />
+ 
+)
